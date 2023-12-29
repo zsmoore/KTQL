1e0027685afcfe0952152ec2e8f0bd9aaa29d1d7
Zachary Moore <zsmoore@zachary-moore.com>
2023-12-29 21:57:46 +0400

    Add java example (#3)

--- scratch/src/test/kotlin/KTQLShould.kt
+++ scratch/src/test/kotlin/KTQLShould.kt
@@ -1,3 +1,4 @@
+import com.zachary_moore.JavaQuery
 import com.zachary_moore.engine.stringify
 import com.zachary_moore.simpleQuery
 import org.junit.jupiter.api.Test
@@ -14,6 +15,13 @@ class KTQLShould {
     @Test
     fun stringifyKTQL() {
         val res = simpleQuery()
+        val str = stringify(res)
+        assertEquals("id, Author{last_name, first_name}, Stats{views, likes, retweets, responses}", str)
+    }
+
+    @Test
+    fun stringifyKTQLJava() {
+        val res = JavaQuery.simpleQuery()
         val str = stringify(res)
         assertEquals("id, Author{last_name, first_name}, Stats{views, likes, retweets, responses}", str)
     }
